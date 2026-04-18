68d843486e7d53e1a17096b210bcc887fcf0db1e
Eshwar <eshwarmp02@gmail.com>
2026-04-18 11:55:09 +0530

    add pubspec, main.dart, app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../core/theme/app_theme.dart';
+import '../features/lessons/screens/home_screen.dart';
+
+class GraamaShaaleApp extends StatelessWidget {
+  const GraamaShaaleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GraamaShaale',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const HomeScreen(),
+    );
+  }
+}
